829b44dfed337c90a255edf8be882d8f5b077808
userA <userA@email.com>
2024-06-03 20:32:44 +0700

    commit

---
+++ src/main/kotlin/net/devtroika/DeepminegalacticDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.devtroika
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object DeepminegalacticDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
